fee16c5cb92b40050f6a2d6a8beaec305f5232a4
JeremyJBrennan <36543625+JeremyJBrennan@users.noreply.github.com>
2020-11-06 01:11:01 -0500

    New version after breaking old
    Now has emerald

---
+++ forgeProject2/src/main/java/com/bedmen/oddc/Odyssey.java
@@ -0,0 +1,45 @@
+package com.bedmen.oddc;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.bedmen.oddc.util.RegistryHandler;
+
+import java.util.stream.Collectors;
+
+@Mod("oddc")
+public class Odyssey
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "oddc";
+    
+    public Odyssey() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        
+        RegistryHandler.init();
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    }
+
+}
