0b7e4f3fc387bff3f30899b26a7468d7b08e54cb
JeremyJBrennan <36543625+JeremyJBrennan@users.noreply.github.com>
2020-11-11 00:41:57 -0500

    Streamlined OreGen a bit
    Also rubies now only spawn in desert

--- src/main/java/world/gen/ModOreGen.java
+++ src/main/java/world/gen/ModOreGen.java
@@ -1,42 +1,49 @@
 package world.gen;
 
-import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.WorldGenRegistries;
+import net.minecraft.block.Block;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.IFeatureConfig;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
 import net.minecraftforge.common.world.BiomeGenerationSettingsBuilder;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import util.RegistryHandler;
-
-import com.bedmen.odyssey.Odyssey;
 
 @Mod.EventBusSubscriber
 public class ModOreGen {
 	
-	public static ConfiguredFeature<?, ?> RUBY_ORE;
+	public static ConfiguredFeature<?, ?> RUBY_ORE_FEATURE;
 	
 	public static void registerOres() {
-		RUBY_ORE = register("rubyOre", Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, RegistryHandler.RUBY_ORE.get().getDefaultState(), 9)).range(64).square().func_242731_b(20));
+		RUBY_ORE_FEATURE = oreGen(64, 9, 20, RegistryHandler.RUBY_ORE);
 	}
 	
 	@SubscribeEvent(priority = EventPriority.HIGHEST)
 	public static void generation(BiomeLoadingEvent event) {
 		BiomeGenerationSettingsBuilder gen = event.getGeneration();
-		if (!(event.getCategory().equals(Biome.Category.NETHER) || event.getCategory().equals(Biome.Category.THEEND))) {
-			gen.withFeature(GenerationStage.Decoration.UNDERGROUND_ORES, RUBY_ORE);
+		
+		if(event.getCategory() == Biome.Category.NETHER) {
+			// Nether Ore
+			
+		} else if (event.getCategory() == Biome.Category.THEEND) {
+			// End Ore
+			
+		} else {
+			//Overworld Ore
+			if(event.getCategory() == Biome.Category.DESERT) {
+				gen.withFeature(GenerationStage.Decoration.UNDERGROUND_ORES, RUBY_ORE_FEATURE);
+			}
 		}
 		
 	}
 	
-	private static <FC extends IFeatureConfig> ConfiguredFeature<FC,?> register(String name, ConfiguredFeature<FC, ?>  cfeat) {
-	      return Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, Odyssey.MOD_ID, cfeat);
+	private static ConfiguredFeature<?, ?> oreGen(int max_y, int size, int count, RegistryObject<Block> oreBlock){
+		return Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, oreBlock.get().getDefaultState(), size)).range(max_y).square().func_242731_b(count);
 	}
 	
 }
