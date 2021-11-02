f8ba898657dd2955884df54cf216a1a0eeda38e8
JeremyJBrennan <36543625+JeremyJBrennan@users.noreply.github.com>
2021-11-02 12:59:07 -0400

    Remove MixinEntityRendererManager.java

--- src/main/java/com/bedmen/odyssey/client/renderer/RenderEvents.java
+++ src/main/java/com/bedmen/odyssey/client/renderer/RenderEvents.java
@@ -1,13 +1,21 @@
 package com.bedmen.odyssey.client.renderer;
 
+import com.bedmen.odyssey.client.renderer.entity.layers.AmuletLayer;
+import com.bedmen.odyssey.client.renderer.entity.layers.OdysseyElytraLayer;
 import com.bedmen.odyssey.client.renderer.entity.layers.OdysseyHeldItemLayer;
+import com.bedmen.odyssey.client.renderer.entity.layers.QuiverLayer;
 import com.bedmen.odyssey.items.QuiverItem;
 import com.bedmen.odyssey.registry.EffectRegistry;
 import com.bedmen.odyssey.util.FogUtil;
+import net.minecraft.client.entity.player.AbstractClientPlayerEntity;
 import net.minecraft.client.renderer.entity.LivingRenderer;
+import net.minecraft.client.renderer.entity.PlayerRenderer;
+import net.minecraft.client.renderer.entity.layers.ElytraLayer;
 import net.minecraft.client.renderer.entity.layers.HeldItemLayer;
+import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.client.renderer.entity.model.IHasArm;
+import net.minecraft.client.renderer.entity.model.PlayerModel;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
@@ -16,7 +24,10 @@ import net.minecraft.util.Hand;
 import net.minecraftforge.client.event.EntityViewRenderEvent;
 import net.minecraftforge.client.event.RenderHandEvent;
 import net.minecraftforge.client.event.RenderLivingEvent;
+import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import java.util.List;
 
 public class RenderEvents {
 
@@ -63,7 +74,35 @@ public class RenderEvents {
                 renderer.layers.set(i, new OdysseyHeldItemLayer<>(renderer));
             }
         }
+    }
 
+    /**
+     * Adjusts PlayerRenderer Layers
+     */
+    @SubscribeEvent
+    public static void RenderHandEventListener(final RenderPlayerEvent.Pre event){
+        PlayerRenderer playerRenderer = event.getRenderer();
+        List<LayerRenderer<AbstractClientPlayerEntity, PlayerModel<AbstractClientPlayerEntity>>> layers = playerRenderer.layers;
+        boolean[] flags = new boolean[3];
+        layers.removeIf((layer) -> layer instanceof ElytraLayer);
+        for(int i = 0; i < layers.size(); i++){
+            if(layers.get(i) instanceof QuiverLayer){
+                flags[0] = true;
+            } else if(layers.get(i) instanceof AmuletLayer){
+                flags[1] = true;
+            } else if(layers.get(i) instanceof OdysseyElytraLayer){
+                flags[2] = true;
+            }
+        }
+        if(!flags[0]){
+            layers.add(new QuiverLayer<>(playerRenderer));
+        }
+        if(!flags[1]){
+            layers.add(new AmuletLayer<>(playerRenderer));
+        }
+        if(!flags[2]){
+            layers.add(new OdysseyElytraLayer<>(playerRenderer));
+        }
     }
 
 }
