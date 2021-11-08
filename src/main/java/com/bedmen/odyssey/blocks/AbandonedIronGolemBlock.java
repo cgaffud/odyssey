06d25d4bb3f0c1155eced7ff6288b71e7fcb1622
JeremyJBrennan <36543625+JeremyJBrennan@users.noreply.github.com>
2021-11-08 15:48:46 -0500

    Fixed Abandoned Iron Golem rotations

--- src/main/java/com/bedmen/odyssey/blocks/AbandonedIronGolemBlock.java
+++ src/main/java/com/bedmen/odyssey/blocks/AbandonedIronGolemBlock.java
@@ -5,17 +5,21 @@ import com.bedmen.odyssey.registry.EntityTypeRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.HorizontalBlock;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.EnumProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.state.properties.DoubleBlockHalf;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.IBooleanFunction;
 import net.minecraft.util.math.shapes.ISelectionContext;
@@ -31,19 +35,36 @@ import javax.annotation.Nullable;
 public class AbandonedIronGolemBlock extends Block implements INeedsToRegisterRenderType {
 
     public static final EnumProperty<DoubleBlockHalf> HALF = BlockStateProperties.DOUBLE_BLOCK_HALF;
-    protected static final VoxelShape SHAPE_UPPER = VoxelShapes.join(box(3,-0.5,4.5,13,5.5,11.5),box(-1,5,2,17,17,13), IBooleanFunction.OR);
-    protected static final VoxelShape SHAPE_LOWER = VoxelShapes.join(box(9.5,0,6,15.5,16,11),box(0.5,0,6,6.5,16,11), IBooleanFunction.OR);
+    public static final DirectionProperty FACING = HorizontalBlock.FACING;
+    protected static final VoxelShape SHAPE_UPPER_NORTH = VoxelShapes.join(box(3,-0.5,4.5,13,5.5,11.5),box(-1,5,2,17,17,13), IBooleanFunction.OR);
+    protected static final VoxelShape SHAPE_UPPER_EAST = VoxelShapes.join(box(4.5,-0.5,4,11.5,5.5,13),box(3,5,-1,14,17,17), IBooleanFunction.OR);
+    protected static final VoxelShape SHAPE_UPPER_SOUTH = VoxelShapes.join(box(3,-0.5,4.5,13,5.5,11.5),box(-1,5,3,17,17,14), IBooleanFunction.OR);
+    protected static final VoxelShape SHAPE_UPPER_WEST = VoxelShapes.join(box(4.5,-0.5,3,11.5,5.5,13),box(2,5,-1,13,17,17), IBooleanFunction.OR);
+    protected static final VoxelShape SHAPE_LOWER_NORTH = VoxelShapes.join(box(9.5,0,6,15.5,16,11),box(0.5,0,6,6.5,16,11), IBooleanFunction.OR);
+    protected static final VoxelShape SHAPE_LOWER_EAST = VoxelShapes.join(box(5,0,9.5,10,16,15.5),box(5,0,0.5,10,16,6.5), IBooleanFunction.OR);
+    protected static final VoxelShape SHAPE_LOWER_SOUTH = VoxelShapes.join(box(9.5,0,5,15.5,16,10),box(0.5,0,5,6.5,16,10), IBooleanFunction.OR);
+    protected static final VoxelShape SHAPE_LOWER_WEST = VoxelShapes.join(box(6,0,9.5,11,16,15.5),box(6,0,0.5,11,16,6.5), IBooleanFunction.OR);
 
     public AbandonedIronGolemBlock(Properties properties) {
         super(properties);
-        this.registerDefaultState(this.stateDefinition.any().setValue(HALF, DoubleBlockHalf.LOWER));
+        this.registerDefaultState(this.stateDefinition.any().setValue(HALF, DoubleBlockHalf.LOWER).setValue(FACING, Direction.NORTH));
     }
 
     public VoxelShape getShape(BlockState blockState, IBlockReader p_220053_2_, BlockPos p_220053_3_, ISelectionContext p_220053_4_) {
         if(blockState.getValue(HALF) == DoubleBlockHalf.UPPER){
-            return SHAPE_UPPER;
+            switch(blockState.getValue(FACING)){
+                case EAST: return SHAPE_UPPER_EAST;
+                case SOUTH: return SHAPE_UPPER_SOUTH;
+                case WEST: return SHAPE_UPPER_WEST;
+                default: return SHAPE_UPPER_NORTH;
+            }
         }
-        return SHAPE_LOWER;
+        switch(blockState.getValue(FACING)){
+            case EAST: return SHAPE_LOWER_EAST;
+            case SOUTH: return SHAPE_LOWER_SOUTH;
+            case WEST: return SHAPE_LOWER_WEST;
+            default: return SHAPE_LOWER_NORTH;
+        }
     }
 
     public BlockState updateShape(BlockState pState, Direction pFacing, BlockState pFacingState, IWorld pLevel, BlockPos pCurrentPos, BlockPos pFacingPos) {
@@ -58,11 +79,12 @@ public class AbandonedIronGolemBlock extends Block implements INeedsToRegisterRe
     @Nullable
     public BlockState getStateForPlacement(BlockItemUseContext pContext) {
         BlockPos blockpos = pContext.getClickedPos();
-        return blockpos.getY() < 255 && pContext.getLevel().getBlockState(blockpos.above()).canBeReplaced(pContext) ? super.getStateForPlacement(pContext) : null;
+        BlockState blockState =  blockpos.getY() < 255 && pContext.getLevel().getBlockState(blockpos.above()).canBeReplaced(pContext) ? super.getStateForPlacement(pContext) : null;
+        return blockState.setValue(FACING, pContext.getHorizontalDirection());
     }
 
     public void setPlacedBy(World pLevel, BlockPos pPos, BlockState pState, LivingEntity pPlacer, ItemStack pStack) {
-        pLevel.setBlock(pPos.above(), this.defaultBlockState().setValue(HALF, DoubleBlockHalf.UPPER), 3);
+        pLevel.setBlock(pPos.above(), this.defaultBlockState().setValue(HALF, DoubleBlockHalf.UPPER).setValue(FACING, pState.getValue(FACING)), 3);
     }
 
     public boolean canSurvive(BlockState pState, IWorldReader pLevel, BlockPos pPos) {
@@ -86,6 +108,9 @@ public class AbandonedIronGolemBlock extends Block implements INeedsToRegisterRe
             double d0 = pState.getValue(HALF) == DoubleBlockHalf.LOWER ? 0d : -1d;
             AbandonedIronGolemEntity abandonedIronGolemEntity = EntityTypeRegistry.ABANDONED_IRON_GOLEM.get().create(pLevel);
             abandonedIronGolemEntity.setPos(pPos.getX() + 0.5d, pPos.getY()+d0, pPos.getZ() + 0.5d);
+            float f0 = pState.getValue(FACING).getOpposite().toYRot();
+            abandonedIronGolemEntity.setYBodyRot(f0);
+            abandonedIronGolemEntity.setYHeadRot(f0);
             pLevel.addFreshEntity(abandonedIronGolemEntity);
         }
 
@@ -96,8 +121,16 @@ public class AbandonedIronGolemBlock extends Block implements INeedsToRegisterRe
         super.playerDestroy(pLevel, pPlayer, pPos, Blocks.AIR.defaultBlockState(), pTe, pStack);
     }
 
+    public BlockState rotate(BlockState state, Rotation rot) {
+        return state.setValue(FACING, rot.rotate(state.getValue(FACING)));
+    }
+
+    public BlockState mirror(BlockState state, Mirror mirrorIn) {
+        return state.rotate(mirrorIn.getRotation(state.getValue(FACING)));
+    }
+
     protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> pBuilder) {
-        pBuilder.add(HALF);
+        pBuilder.add(HALF, FACING);
     }
 
     @Override
