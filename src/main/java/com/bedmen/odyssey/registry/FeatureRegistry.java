1adebf0717c80ac71433634ef858b4502c843666
JeremyJBrennan <36543625+JeremyJBrennan@users.noreply.github.com>
2022-01-05 17:49:46 -0500

    Added Underground Treasure Chests

--- src/main/java/com/bedmen/odyssey/registry/FeatureRegistry.java
+++ src/main/java/com/bedmen/odyssey/registry/FeatureRegistry.java
@@ -1,8 +1,10 @@
 package com.bedmen.odyssey.registry;
 
 import com.bedmen.odyssey.Odyssey;
+import com.bedmen.odyssey.world.gen.feature.TreasureChestFeature;
 import com.bedmen.odyssey.world.gen.feature.tree.CornerLeafTreeFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -11,12 +13,12 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class FeatureRegistry {
 
-    public static DeferredRegister<Feature<?>> FEATURES = DeferredRegister.create(ForgeRegistries.FEATURES , Odyssey.MOD_ID);
+    public static DeferredRegister<Feature<?>> FEATURES = DeferredRegister.create(ForgeRegistries.FEATURES, Odyssey.MOD_ID);
 
     public static void init() {
         FEATURES.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
     public static final RegistryObject<Feature<TreeConfiguration>> CORNER_LEAF_TREE = FEATURES.register("corner_leaf_tree", () -> new CornerLeafTreeFeature(TreeConfiguration.CODEC));
-    //public static final RegistryObject<Feature<NoneFeatureConfiguration>> WEAVER_COLONY = FEATURES.register("weaver_colony", () -> new WeaverColonyFeature(NoneFeatureConfiguration.CODEC));
+    public static final RegistryObject<Feature<NoneFeatureConfiguration>> TREASURE_CHEST = FEATURES.register("treasure_chest", () -> new TreasureChestFeature(NoneFeatureConfiguration.CODEC));
 }
