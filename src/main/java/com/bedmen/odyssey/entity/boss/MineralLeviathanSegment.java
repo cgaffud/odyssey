2cc1bee97aad5de874b2898463b02befdf3ba6fc
JeremyJBrennan <36543625+JeremyJBrennan@users.noreply.github.com>
2022-01-30 14:15:58 -0500

    ML fix

--- src/main/java/com/bedmen/odyssey/entity/boss/MineralLeviathanSegment.java
+++ src/main/java/com/bedmen/odyssey/entity/boss/MineralLeviathanSegment.java
@@ -7,6 +7,7 @@ import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.server.level.ServerBossEvent;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.Mth;
@@ -119,7 +120,7 @@ public abstract class MineralLeviathanSegment extends Boss implements IEntityAdd
                 i = 4;
                 break;
         }
-        return i * this.getBossEvent().getPlayers().size();
+        return i * this.getNearbyPlayerNumber();
     }
 
     public void addAdditionalSaveData(CompoundTag compoundNBT) {
