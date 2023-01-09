3e32945ba1ac09922ac0804ec2f85109c16aafa2
JeremyJBrennan <36543625+JeremyJBrennan@users.noreply.github.com>
2023-01-09 01:01:52 -0500

    Add climate aspects

--- src/main/java/com/bedmen/odyssey/aspect/Aspects.java
+++ src/main/java/com/bedmen/odyssey/aspect/Aspects.java
@@ -1,12 +1,16 @@
 package com.bedmen.odyssey.aspect;
 
 import com.bedmen.odyssey.tags.OdysseyEntityTags;
+import com.bedmen.odyssey.util.BiomeUtil;
 import com.bedmen.odyssey.util.StringUtil;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobType;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.biome.Biome;
 
 public class Aspects {
     public static final Aspect DAMAGE_AGAINST_ARTHROPOD = new TargetConditionalMeleeAspect("damage_against_arthropod", livingEntity -> livingEntity.getMobType() == MobType.ARTHROPOD);
@@ -19,6 +23,9 @@ public class Aspects {
     public static final Aspect COBWEB_CHANCE = new Aspect("cobweb_chance", aspectInstance -> new TranslatableComponent("aspect.oddc."+aspectInstance.aspect.id, StringUtil.percentFormat(aspectInstance.strength)));
     public static final Aspect SOLAR_STRENGTH = new EnvironmentConditionalMeleeAspect("solar_strength", Aspects::getSunBoost);
     public static final Aspect LUNAR_STRENGTH = new EnvironmentConditionalMeleeAspect("lunar_strength", Aspects::getMoonBoost);
+    public static final Aspect BOTANICAL_STRENGTH = new EnvironmentConditionalMeleeAspect("botanical_strength", Aspects::getHotHumidBoost);
+    public static final Aspect SCORCHED_STRENGTH = new EnvironmentConditionalMeleeAspect("scorched_strength", Aspects::getHotHumidBoost);
+    public static final Aspect WINTERY_STRENGTH = new EnvironmentConditionalMeleeAspect("wintery_strength", Aspects::getHotHumidBoost);
 
     private static float getSunBoost(BlockPos pos, Level level) {
         long time = level.getDayTime() % 24000L;
@@ -31,5 +38,31 @@ public class Aspects {
 
     private static float getSkyBoost(BlockPos pos, Level level) {
         return (level.canSeeSky(pos) && !level.isThundering() && !level.isRaining() && (level.dimension() == Level.OVERWORLD)) ? 1.0f : 0.0f;
+    }
+
+    public static float getHotHumidBoost(BlockPos pos, Level level) {
+        return Mth.sqrt(getHotBoost(pos, level) * getHumidBoost(pos, level));
+    }
+
+    public static float getHotDryBoost(BlockPos pos, Level level) {
+        return Mth.sqrt(getHotBoost(pos, level) * getDryBoost(pos, level));
+    }
+
+    public static float getHotBoost(BlockPos pos, Level level) {
+        return Mth.clamp(BiomeUtil.getClimate(level.getBiome(pos)).temperature, 0.0f, 1.0f);
+    }
+
+    public static float getColdBoost(BlockPos pos, Level level) {
+        return 1.0f - getHotBoost(pos, level);
+    }
+
+    public static float getHumidBoost(BlockPos pos, Level level) {
+        Holder<Biome> biomeHolder = level.getBiome(pos);
+        Biome biome = level.getBiome(pos).value();
+        return level.isRaining() && biome.getPrecipitation() == Biome.Precipitation.RAIN ? 1.0f : Mth.clamp(BiomeUtil.getClimate(biomeHolder).downfall, 0.0f, 1.0f);
+    }
+
+    public static float getDryBoost(BlockPos pos, Level level) {
+        return 1f - getHumidBoost(pos, level);
     }
 }
