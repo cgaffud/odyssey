c75ea31668d8fc719f59113458c3808db0877a25
Christoph Gaffud <christoph.gaffud@gmail.com>
2024-02-19 00:42:47 -0800

    Add delayed Subentity spawning

--- src/main/java/com/bedmen/odyssey/entity/boss/BossMaster.java
+++ src/main/java/com/bedmen/odyssey/entity/boss/BossMaster.java
@@ -13,16 +13,21 @@ import net.minecraft.world.entity.Entity.RemovalReason;
 
 public abstract class BossMaster extends Boss {
     public static final String SUB_ENTITIES_TAG = "SubEntities";
+    public static final String HAS_SPAWNED_SUB_ENTITIES_TAG = "SpawnedSubEntities";
+    private boolean hasSpawnedSubentities;
 
     protected BossMaster(EntityType<? extends BossMaster> entityType, Level level) {
         super(entityType, level);
         this.noPhysics = true;
+        this.hasSpawnedSubentities = false;
         this.setNoGravity(true);
     }
 
     public void onAddedToWorld() {
-        if(!this.level.isClientSide) {
+        if(!this.level.isClientSide && !this.shouldDelaySpawn()) {
             this.spawnSubEntities();
+            System.out.println(this.position());
+            this.hasSpawnedSubentities = true;
         }
     }
 
@@ -34,16 +39,25 @@ public abstract class BossMaster extends Boss {
 
     public void serverTick() {
         super.serverTick();
+        if (this.shouldDelaySpawn() && !this.hasSpawnedSubentities && (this.tickCount <= this.spawnDelayTickLength())) {
+            System.out.println(this.position());
+        }
+        if (this.shouldDelaySpawn() && !this.hasSpawnedSubentities && (this.tickCount > this.spawnDelayTickLength())) {
+            this.hasSpawnedSubentities = true;
+            this.spawnSubEntities();
+        }
         this.performMasterMovement();
     }
 
     public void addAdditionalSaveData(@NotNull CompoundTag compoundTag) {
         super.addAdditionalSaveData(compoundTag);
+        compoundTag.putBoolean(HAS_SPAWNED_SUB_ENTITIES_TAG, this.hasSpawnedSubentities);
         compoundTag.put(SUB_ENTITIES_TAG, this.saveSubEntities());
     }
 
     public void readAdditionalSaveData(@NotNull CompoundTag compoundTag) {
         super.readAdditionalSaveData(compoundTag);
+        this.hasSpawnedSubentities = compoundTag.getBoolean(HAS_SPAWNED_SUB_ENTITIES_TAG);
         // subEntitiesTag could be empty (but not null) if there was no CompoundTag found for SUB_ENTITIES_TAG
         CompoundTag subEntitiesTag = compoundTag.getCompound(SUB_ENTITIES_TAG);
         this.loadSubEntities(subEntitiesTag);
@@ -74,4 +88,13 @@ public abstract class BossMaster extends Boss {
     public abstract CompoundTag saveSubEntities();
 
     public abstract void loadSubEntities(CompoundTag compoundTag);
+
+    // Delay spawning injection:
+    protected boolean shouldDelaySpawn() {
+        return false;
+    }
+
+    protected int spawnDelayTickLength() {
+        return 0;
+    }
 }
